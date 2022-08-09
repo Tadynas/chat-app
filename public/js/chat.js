3179b565ab126ce10d0b62006f9f805533c8805c
Tadynas <griciustadas@gmail.com>
2022-08-09 15:17:25 +0300

    create join page

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,6 +11,8 @@ const messages = document.querySelector('#messages')
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationTemplate = document.querySelector('#location-message-template').innerHTML
 
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
 
 socket.on('message', (message) => {
     console.log(message)
@@ -68,3 +70,5 @@ locationButton.addEventListener('click', () => {
         })
     })
 })
+
+socket.emit('join', { username, room })
