860547fde9330fa9fe4eb0f5e4ac1f7c74ae7e25
Tadynas <griciustadas@gmail.com>
2022-08-09 15:38:57 +0300

    finish user functions

--- src/utils/users.js
+++ src/utils/users.js
@@ -27,10 +27,25 @@ const addUser = ({ id, username, room }) => {
     return { user }
 }
 
+const getUser = (id) => {
+    return users.find((user) => user.id === id)
+}
+
+const getUsersInRoom = (room) => {
+    return users.filter((user) => user.room === room)
+}
+
 const removeUser = (id) => {
     const index = users.findIndex((user) => user.id === id)
 
     if (index !== -1) {
         return users.splice(index, 1)[0]
     }
+}
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
 }
