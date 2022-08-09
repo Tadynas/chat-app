518f0f21c0aad19c345ad31f2512380f6881a887
Tadynas <griciustadas@gmail.com>
2022-08-09 15:50:08 +0300

    track users joining and leaving

--- public/js/chat.js
+++ public/js/chat.js
@@ -71,4 +71,10 @@ locationButton.addEventListener('click', () => {
     })
 })
 
-socket.emit('join', { username, room })
+socket.emit('join', { username, room }, (error) => {
+    console.log(error)
+    if (error) {
+        alert(error)
+        location.href = '/'
+    }
+})
