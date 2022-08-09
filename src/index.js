2041d88e2f9534c97e38936b9054b0bb532354cb
Tadynas <griciustadas@gmail.com>
2022-08-09 16:01:49 +0300

    tracking users joining and leaving chat

--- src/index.js
+++ src/index.js
@@ -31,25 +31,29 @@ io.on('connection', (socket) => {
 
         socket.join(user.room)
 
-        socket.emit('message', generateMessage('Welcome!'))
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+        socket.emit('message', generateMessage('Admin', 'Welcome!'))
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`))
 
         callback()
     })
 
     socket.on('sendMessage', (message, callback) => {
+
         const filter = new Filter()
 
         if(filter.isProfane(message)) {
             return callback('Profanity is not allowed!')
         }
 
-        io.to('global').emit('message', generateMessage(message))
+        const user = getUser(socket.id)
+
+        io.to(user.room).emit('message', generateMessage(user.username, message))
         callback()
     })
 
     socket.on('sendLocation', (location, callback) => {
-        socket.emit('locationMessage', generateLocationMessage(`https://www.google.com/maps?q=${location.latitude},${location.longitude}`))
+        const user = getUser(socket.id)
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://www.google.com/maps?q=${location.latitude},${location.longitude}`))
         callback()
     })
 
@@ -57,7 +61,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id)
 
         if (user) {
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left the chat!`))
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left the chat!`))
         }
     })
 })
